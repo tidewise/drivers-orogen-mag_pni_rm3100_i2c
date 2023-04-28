4e591660db78dd57a082bea3a87ec9b27658e9ac
kapeps <rodrigo.kappes@student-cs.fr>
2023-04-28 14:47:42 -0300

    initial commit by rock-create-orogen

---
+++ mag_pni_rm3100_i2cTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef mag_pni_rm3100_i2c_TYPES_HPP
+#define mag_pni_rm3100_i2c_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace mag_pni_rm3100_i2c {
+}
+
+#endif
+
